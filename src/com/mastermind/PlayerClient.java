739f15f29df1b0c5d8a07b164f097b7e881f4230
Tapan Trivedi <tapan_trivedi_4u@yahoo.co.in>
2020-05-10 17:20:38 -0700

    Add Client Player

---
+++ src/com/mastermind/PlayerClient.java
@@ -0,0 +1,11 @@
+package com.mastermind.Client.Player;
+
+import com.mastermind.Player;
+
+public class PlayerClient {
+    public static void main(String[] args) {
+        Player  play = new Player();
+        play.userInput();
+
+    }
+}
