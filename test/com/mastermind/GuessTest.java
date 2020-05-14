61f50b7e204dff21aac80527c90cdcbd66b6487a
Michael Lennerblom <mlennerblom@gmail.com>
2020-05-13 20:57:24 -0700

    updated tets and removed extra comments

--- test/com/mastermind/GuessTest.java
+++ test/com/mastermind/GuessTest.java
@@ -1,12 +1,31 @@
 package com.mastermind;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class GuessTest {
+    Map<Integer,String> map;
+    @Before
+    public void setUp() {
+        List<String> colors = Arrays.asList("R", "O", "G", "Y", "B", "P", "S", "W");
+         map = new HashMap<>();
+         int counter = 0;
+        for(String color : colors) {
+            map.put(counter,color);
+            counter++;
+        }
+    }
 
     @Test
     public void testsSetGuess() {
+        Guess guess = new Guess(map);
+        assertEquals(8, guess.getGuess().size());
     }
 }
